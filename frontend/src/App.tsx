061872a378adc6733ca63195c4d42493cdea0470
maxim.yo <mmitin2005@mail.ru>
2026-04-05 17:30:02 +0300

    фронтенд окружение

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <section className="flex justify-center items-center h-screen">
+      <h1 className="text-6xl text-white">
+        REACT + VITE + TAILWIND
+      </h1>
+    </section>
+  );
+}
+
+export default App;
