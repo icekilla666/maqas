93cbb62cdfdd043992f386d1f1adc71198723795
olesyayo <sev.olesya@mail.ru>
2026-04-05 19:47:41 +0300

    nginx

--- backend/main.py
+++ backend/main.py
@@ -17,10 +17,7 @@ app = FastAPI(
     )
 
 origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
+    "http://localhost:5173",
 ]
 
 app.add_middleware(
