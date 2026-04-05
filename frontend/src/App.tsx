dfdb3a10af4c02ee98cec081d916ffe51766f5ec
olesyayo <sev.olesya@mail.ru>
2026-04-05 18:44:55 +0300

    integration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <section className="flex justify-center items-center h-screen">
       <h1 className="text-6xl text-white">
-        REACT + VITE + TAILWIND
+        REACT + VITE + TAILWIND + FASTAPI
       </h1>
     </section>
   );
