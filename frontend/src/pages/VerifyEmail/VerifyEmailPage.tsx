a85b05e17c77c897865b8869fc6d984c945afdba
maxim.yo <mmitin2005@mail.ru>
2026-04-26 19:34:40 +0300

    обработка запроса и ошибок для верификации

--- frontend/src/pages/VerifyEmail/VerifyEmailPage.tsx
+++ frontend/src/pages/VerifyEmail/VerifyEmailPage.tsx
@@ -1,32 +1,55 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import VerifyEmailWrapper from "./components/VerifyEmailWrapper";
 import { LOGIN_PAGE } from "@/utils/constants";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { authApi } from "@/services/auth.api";
+import axios from "axios";
 
 const VerifyEmailPage = () => {
+  const [status, setStatus] = useState<"error" | "success" | null>(null);
+  const [error, setError] = useState("");
   const [searchParams] = useSearchParams();
   const token = searchParams.get("token");
   const navigate = useNavigate();
   useEffect(() => {
-    if (token) {
-      const navigateTimeout = setTimeout(() => {
-        navigate(LOGIN_PAGE);
-      }, 3000);
-      return () => clearTimeout(navigateTimeout);
-    }
+    if (!token) return;
+
+    const checkedToken = async () => {
+      try {
+        await authApi.verifyEmail(token);
+        setStatus("success");
+      } catch (error: unknown) {
+        if (axios.isAxiosError(error)) {
+          setError(
+            error.response?.data?.detail ||
+              "Неизвестная ошибка! Попробуйте позже",
+          );
+        } else {
+          setError("Неизвестная ошибка! Попробуйте позже");
+        }
+        setStatus("error");
+      }
+    };
+    checkedToken();
+    const navigateTimeout = setTimeout(() => {
+      navigate(LOGIN_PAGE);
+    }, 3000);
+    return () => clearTimeout(navigateTimeout);
   }, [navigate, token]);
+
   return (
     <section className="h-svh flex justify-center items-center px-7">
-      {token ? (
+      {status === "success" && (
         <VerifyEmailWrapper
           title="Регистрация прошла успешно!"
           text={`Ваш email подтверждён.\nПеренаправление на страницу входа...`}
           variant="success"
         />
-      ) : (
+      )}
+      {status === "error" && (
         <VerifyEmailWrapper
-          title="Ссылка недействительна"
-          text="Повторите попытку регистрации"
+          title="Возникла ошибка!"
+          text={error}
           button="отправить сообщение повторно"
           variant="error"
         />
