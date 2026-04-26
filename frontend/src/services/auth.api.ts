697364ad409ec4189965f34a88c2be52da1b82e0
maxim.yo <mmitin2005@mail.ru>
2026-04-26 19:51:08 +0300

    временно убрал серверные ошибки

--- frontend/src/services/auth.api.ts
+++ frontend/src/services/auth.api.ts
@@ -12,7 +12,7 @@ export const authApi = {
     return api.post("/api/auth/register", data);
   },
   verifyEmail: (token: string) => {
-    return api.get(`/api/auth/verify-email?${token}`);
+    return api.get(`/api/auth/verify-email?token=${token}`);
   },
   login: (data: LoginData) => {
     return api.post("/api/auth/login", data);
