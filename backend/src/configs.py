1d87570d33b100bb10b8fc8dba3a847a867f5dd1
olesyayo <sev.olesya@mail.ru>
2026-05-10 16:33:25 +0300

    configs fix

--- backend/src/configs.py
+++ backend/src/configs.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     ALGORITHM: str 
     ACCESS_TOKEN_EXPIRE_MINUTES: int
     REFRESH_TOKEN_EXPIRE_DAYS: int 
-    EMAIL_VERIFY_TOKEN_EXPIRE_MiNUTES: int 
+    EMAIL_VERIFY_TOKEN_EXPIRE_MINUTES: int 
 
     VERIFY_EMAIL_URL: str
     
